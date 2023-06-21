0c722b68bbecfce4994bc063f5dff34b950da733
marionbarker <marionbarker@earthlink.net>
2023-06-20 22:05:03 -0700

    update generate tools

--- generate_patches/configure_alias_names.sh
+++ generate_patches/configure_alias_names.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #   It MUST be run using source in order to configure alias
-export local_patch_path="/Users/marion/dev/git_local/Code/patchrepo"
+export local_patch_path="/Users/marion/dev/git_local/Code/customization"
 
 alias reset_workspace="${local_patch_path}/generate_patches/reset_workspace.sh"
 alias generate_main="${local_patch_path}/generate_patches/main.sh"
+alias generate_dev="${local_patch_path}/generate_patches/dev.sh"
 
 # method
 #  start in directory with LoopWorkspace main branch
